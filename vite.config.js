1e3664824930b57ef87dc849585d5d1e2e03e673
Bruno Costa <brunocosta0495@gmail.com>
2026-08-31 18:36:06 -0300

    fix: added automatic open vite on chrome

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,20 @@ import { fileURLToPath } from 'url';
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
-   base: '/',
+  base: '/',
 
-   build: {
+  build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-            main: resolve(__dirname, 'index.html'),
-            outro: resolve(__dirname, 'outro/index.html'),
+        main: resolve(__dirname, 'index.html'),
+        outro: resolve(__dirname, 'outro/index.html'),
       },
     },
   },
+
+  server: {
+    open: true,
+    browser: 'chrome'
+  }
 });
